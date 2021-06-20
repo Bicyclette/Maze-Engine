4aa5c07d4854dbc10fec8b95c130dbdd7b64b8aa
Bicyclette <mathias.velo@gmail.com>
2021-06-20 05:50:57 +0200

    Readme first edit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Bugs
+* Problem with shadowed areas with SSAO enabled.
+* Enabling skybox prevents any objects of any scene to be visible after draw calls were made.
+
+# To do
+**graphics**
+* IBL (diffuse & specular)
+* OIT
+
+**physics**
+* Character kinematic controller
+* Fix GImpact collision shape
+
+**user interface**
+* OpenGL based user interface (buttons, menus, layout)
+* process for main client, and process for game
+
+**network**
+* code a client server chat with asio or something else
+* implement chat in user interface
+* send matrices over the network to move a character on two clients communicating over the internet
+
+**game design**
+* Write precise game specifications, goals, story etc.
+* Draw some level artworks
+* Character design
+
--- src/game.cpp
+++ src/game.cpp
@@ -40,13 +40,13 @@ Game::Game(int clientWidth, int clientHeight) :
 	scenes.at(scenes.size()-1)->addPointLight(glm::vec3(-1.5f, 3.0f, -10.0f), glm::vec3(0.25f), glm::vec3(10.0f), glm::vec3(1.0f), 1.0f, 0.14f, 0.07f);
 	scenes.at(scenes.size()-1)->addPointLight(glm::vec3(1.5f, 3.0f, -10.0f), glm::vec3(0.025f), glm::vec3(10.0f), glm::vec3(1.0f), 1.0f, 0.045f, 0.0075f);
 */	
-	scenes.at(scenes.size()-1)->addDirectionalLight(glm::vec3(5.0f, 20.0f, 8.0f), glm::vec3(0.025f), glm::vec3(10.0f), glm::vec3(1.0f), glm::vec3(-0.5f, -1.0f, -0.75f));
+	//scenes.at(scenes.size()-1)->addDirectionalLight(glm::vec3(5.0f, 20.0f, 8.0f), glm::vec3(0.025f), glm::vec3(5.0f), glm::vec3(1.0f), glm::vec3(-0.5f, -1.0f, -0.75f));
 	//scenes.at(scenes.size()-1)->addSpotLight(glm::vec3(0.0f, 20.0f, 0.0f), glm::vec3(0.025f), glm::vec3(400.0f), glm::vec3(1.0f), glm::vec3(0.0f, -1.0f, 0.3f), 25.0f, 30.0f);
 	//scenes.at(scenes.size()-1)->addPointLight(glm::vec3(3.0f, 5.0f, -3.0f), glm::vec3(2.5f), glm::vec3(50.0f), glm::vec3(10.0f), 1.0f, 0.045f, 0.0075f);
 	//scenes.at(scenes.size()-1)->addDirectionalLight(glm::vec3(1.0f, 12.0f, 4.0f), glm::vec3(0.025f), glm::vec3(10.0f), glm::vec3(1.0f), glm::vec3(-0.5f, -1.0f, -1.25f));
 	//scenes.at(scenes.size()-1)->addDirectionalLight(glm::vec3(2.0f, 5.0f, -1.0f), glm::vec3(0.025f), glm::vec3(0.5f), glm::vec3(1.0f), glm::vec3(-0.5f, -0.35f, 0.4f));
 	
-	//scenes.at(scenes.size()-1)->addPointLight(glm::vec3(2.0f, 3.5f, 5.0f), glm::vec3(0.025f), glm::vec3(5.0f), glm::vec3(1.0f), 1.0f, 0.045f, 0.0075f);
+	scenes.at(scenes.size()-1)->addPointLight(glm::vec3(2.0f, 7.0f, 5.0f), glm::vec3(0.025f), glm::vec3(10.0f), glm::vec3(1.0f), 1.0f, 0.045f, 0.0075f);
 	//scenes.at(scenes.size()-1)->addPointLight(glm::vec3(0.0f, 2.5f, 7.0f), glm::vec3(0.025f), glm::vec3(5.0f), glm::vec3(1.0f), 1.0f, 0.045f, 0.0075f);
 	//scenes.at(scenes.size()-1)->addPointLight(glm::vec3(0.0f, 7.5f, 0.0f), glm::vec3(0.025f), glm::vec3(5.0f), glm::vec3(1.0f), 1.0f, 0.045f, 0.0075f);
 	
